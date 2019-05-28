da42bad6de2e60f4f8313cac98f37c3c1127ab24
Yihong <isopreneshi@gmail.com>
2019-05-28 05:36:51 -0400

    Site updated: 2019-05-28 05:36:50

---
+++ dist/music.js
@@ -0,0 +1,12 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "Sparrow",
+        artist: 'Adam Hurst',
+        url: 'http://worldcello.bandcamp.com/album/elegy'
+        }
+    ]
+});
